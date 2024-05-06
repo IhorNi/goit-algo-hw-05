ee3646261f8eb9784a743674065bbd592b44b7a7
Ihor Nikolskyi <ihor.nikolskyi@gmail.com>
2024-05-06 18:15:39 +0300

    feat: task_2 add binary search function

---
+++ task_2.py
@@ -0,0 +1,34 @@
+def binary_search_with_iterations(arr, target):
+    left, right = 0, len(arr) - 1
+    iterations = 0
+    upper_bound = None
+
+    while left <= right:
+        iterations += 1
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            upper_bound = arr[mid]
+            break
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+            upper_bound = arr[mid]
+
+    # If target is greater than all elements
+    if upper_bound is None and arr and target > arr[-1]:
+        upper_bound = float('inf')
+
+    return iterations, upper_bound
+
+
+examples = [
+    ([1.1, 2.2, 3.3, 4.4, 5.5], 3.3),
+    ([1.1, 2.2, 3.3, 4.4, 5.5], 4.0),
+    ([1.1, 2.2, 3.3, 4.4, 5.5], 12.0),
+]
+
+for array, target in examples:
+    result = binary_search_with_iterations(array, target)
+    print(f"Find {target} in {array}: {result}")
